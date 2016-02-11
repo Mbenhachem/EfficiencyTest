cea1d303391aad299498fd8c1f29651a07965c6d
Mbenhachem <mbenhachem@efficiency-mc.com>
2016-02-11 16:56:48 +0000

    Revert "modification test"
    This reverts commit 7e46da04b67fd3b91cdca1cbe4e406da25383fc7.

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,6 @@
 //#include "CapFloorEfficiency.hpp"
 #include "SwaptionEfficiency.hpp"
 
-//modificationTest
-
 
 int main(int, char*[]) {
 	std::cout << " Produits disponibles : Cap/Floor, Swaption  " << std::endl;
